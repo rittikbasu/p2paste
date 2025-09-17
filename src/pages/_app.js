04f596801695d2903e591a42297553b42d07d9dd
rittikbasu <irittik@gmail.com>
2025-09-18 03:15:24 +0530

    chore: add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,25 @@ const geistMono = Geist_Mono({
 export default function App({ Component, pageProps }) {
   return (
     <div className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
+      <Head>
+        <title>P2Paste</title>
+        <meta
+          name="description"
+          content="Realtime, peer‑to‑peer pastebin. No servers. No database. No bullshit."
+        />
+        <meta property="og:title" content="P2Paste" />
+        <meta
+          property="og:description"
+          content="Realtime, peer‑to‑peer pastebin. No servers. No database. No bullshit."
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="P2Paste" />
+        <meta
+          name="twitter:description"
+          content="Realtime, peer‑to‑peer pastebin. No servers. No database. No bullshit."
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
