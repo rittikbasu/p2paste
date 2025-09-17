5b90be21b5151bb9225aa641db983148cb46c19e
rittikbasu <irittik@gmail.com>
2025-09-17 22:25:18 +0530

    feat: add meta tags for browser theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="theme-color" content="#0a0a0a" />
+        <meta name="color-scheme" content="dark" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="msapplication-TileColor" content="#0a0a0a" />
       </Head>
       <body className="antialiased">
         <Main />
